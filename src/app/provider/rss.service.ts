e40d548d9a79a4d545f30d0114987309f4b3a8ef
Alejandro <almr193@hotmail.com>
2017-09-08 12:28:36 -0500

    Se cambian puertos y direciones IP, verson estable

--- src/app/provider/rss.service.ts
+++ src/app/provider/rss.service.ts
@@ -6,40 +6,43 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class RssService {
 
+  address = "172.24.100.104";
+  port = "8080";
+  
   constructor(private http: Http) { }
 
   getRssWired() {
-    return this.http.get('http://localhost:8888/RSS_WIRED')
+    return this.http.get('http://'+this.address+':'+this.port+'/RSS_WIRED')
       .map(res => res.json().rss.channel.item)
       .toPromise()
   }
 
   getRssLf() {
-    return this.http.get('http://localhost:8888/RSS_LIFEH')
+    return this.http.get('http://'+this.address+':'+this.port+'/RSS_LIFEH')
       .map(res => res.json().rss.channel.item)
       .toPromise()
   }
 
   getRssBbc() {
-    return this.http.get('http://localhost:8888/RSS_BBC')
+    return this.http.get('http://'+this.address+':'+this.port+'/RSS_BBC')
       .map(res => res.json().rss.channel.item)
       .toPromise()
   }
 
   getRssWiredXQFilter(title:any, description:any, category:any) {
-    return this.http.get('http://localhost:8888/RSS_WIRED_XQ/'+title+"/"+description+"/"+category)
+    return this.http.get('http://'+this.address+':'+this.port+'/RSS_WIRED_XQ/'+title+"/"+description+"/"+category)
       .map(res => res.json().rss.channel.item)
       .toPromise()
   }
 
   getRssLfXQFilter(title:any, description:any, category:any) {
-    return this.http.get('http://localhost:8888/RSS_LIFEH_XQ/'+title+"/"+description+"/"+category)
+    return this.http.get('http://'+this.address+':'+this.port+'RSS_LIFEH_XQ/'+title+"/"+description+"/"+category)
       .map(res => res.json().rss.channel.item)
       .toPromise()
   }
 
   getRssBbcXQFilter(title:any, description:any, category:any) {
-    return this.http.get('http://localhost:8888/RSS_BBC_XQ/'+title+"/"+description+"/"+category)
+    return this.http.get('http://'+this.address+':'+this.port+'/RSS_BBC_XQ/'+title+"/"+description+"/"+category)
       .map(res => res.json().rss.channel.item)
       .toPromise()
   }
