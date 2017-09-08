f5da5ff9ed54e2ae52e858ae3ddaeb17367e3622
Asistente <Asistente@ASISTENTE-172.virtual.uniandes.edu.co>
2017-09-07 20:14:15 -0500

    Adición de ruteo para la pagina de scrapy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { BackService } from './provider/back.service';
-import { RssService } from './provider/rss.service';
-import { RegexTitlePipe } from './pipe/regex-title.pipe';
-import { RegexCategoryPipe } from './pipe/regex-category.pipe';
+
 
 @Component({
   selector: 'app-root',
@@ -10,144 +7,11 @@ import { RegexCategoryPipe } from './pipe/regex-category.pipe';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  profesores = [];
+  
 
-  rssWired = [];
-  rssWiredLen;
-
-  rssLifeH = [];
-  rssLifeHLen;
-
-  rssBbc = [];
-  rssBbcLen;
-
-
-
-  rssWiredRegex = [];
-  rssWiredRegexLen;
-
-  rssLifeHRegex = [];
-  rssLifeHRegexLen;
-
-  rssBbcRegex = [];
-  rssBbcRegexLen;
-
-
-
-  rssWiredXQ = [];
-  rssWiredXQLen;
-
-  rssLifeHXQ = [];
-  rssLifeHXQLen;
-
-  rssBbcXQ = [];
-  rssBbcXQLen;
-
-  title = "";
-  description = "";
-  category = "";
-
-  constructor(private pipeRegexCat: RegexCategoryPipe, private pipeRegex: RegexTitlePipe, private backservice: BackService, private rss: RssService) {
+  constructor() {
 
   }
 
-
-  ngOnInit() {
-    //this.backservice.getUsers().then(prof => this.profesores = prof);
-    this.rss.getRssWired().then(rss => { this.rssWired = rss; this.rssWiredLen = this.rssWired.length });
-    this.rss.getRssWired().then(rss => { this.rssWiredRegex = rss; this.rssWiredRegexLen = this.rssWiredRegex.length });
-    this.rss.getRssWired().then(rss => { this.rssWiredXQ = rss; this.rssWiredXQLen = this.rssWiredXQ.length });
-
-    this.rss.getRssBbc().then(rss => { this.rssBbc = rss; this.rssBbcLen = this.rssBbc.length });
-    this.rss.getRssBbc().then(rss => { this.rssBbcRegex = rss; this.rssBbcRegexLen = this.rssBbcRegex.length });
-    this.rss.getRssBbc().then(rss => { this.rssBbcXQ = rss; this.rssBbcXQLen = this.rssBbcXQ.length });
-
-    this.rss.getRssLf().then(rss => { this.rssLifeH = rss; this.rssLifeHLen = this.rssLifeH.length });
-    this.rss.getRssLf().then(rss => { this.rssLifeHRegex = rss; this.rssLifeHRegexLen = this.rssLifeHRegex.length });
-    this.rss.getRssLf().then(rss => { this.rssLifeHXQ = rss; this.rssLifeHXQLen = this.rssLifeHXQ.length });
-
-
-  }
-
-  filter() {
-
-    let wired = this.rssWired;
-    let lifeh = this.rssLifeH;
-    let bbc = this.rssBbc;
-
-    if (this.title.trim() === "" && this.description.trim() === "" && this.category.trim() === "") {
-      alert("Debe escribir algún parámetro de búsqueda");
-      this.clean();
-    }
-    else {
-
-      // REGEX
-      this.rssWiredRegex = this.pipeRegexCat.transform(this.pipeRegex.transform(this.pipeRegex.transform(wired, "description", this.description.toLowerCase()), "title", this.title.toLowerCase()), "category", this.category);
-      this.rssLifeHRegex = this.pipeRegexCat.transform(this.pipeRegex.transform(this.pipeRegex.transform(lifeh, "description", this.description.toLowerCase()), "title", this.title.toLowerCase()), "category", this.category);
-      this.rssBbcRegex = this.pipeRegexCat.transform(this.pipeRegex.transform(this.pipeRegex.transform(bbc, "description", this.description.toLowerCase()), "title", this.title.toLowerCase()), "category", this.category);
-
-      this.rssWiredRegexLen = this.rssWiredRegex.length
-      this.rssLifeHRegexLen = this.rssLifeHRegex.length
-      this.rssBbcRegexLen = this.rssBbcRegex.length
-
-      // XQuery (Elementree)
-      this.rss.getRssWiredXQFilter(this.title, this.description, this.category)
-        .then(newRss => {
-          if (newRss.length === undefined) {
-            this.rssWiredXQ = [];
-            this.rssWiredXQ.push(newRss);
-          }
-          else {
-            this.rssWiredXQ = newRss;
-          }
-          this.rssWiredXQLen = this.rssWiredXQ.length;
-
-        })
-        .then(undefined, (error) => { this.rssWiredXQ.length = 0; this.rssWiredXQLen = this.rssWiredXQ.length; });
-
-      this.rss.getRssLfXQFilter(this.title, this.description, this.category)
-        .then(newRss => {
-          if (newRss.length === undefined) {
-            this.rssLifeHXQ = [];
-            this.rssLifeHXQ.push(newRss);
-          }
-          else {
-            this.rssLifeHXQ = newRss;
-          }
-          this.rssLifeHXQLen = this.rssLifeHXQ.length;
-        })
-        .then(undefined, (error) => { this.rssLifeHXQ.length = 0; this.rssLifeHXQLen = this.rssLifeHXQ.length; });
-
-      this.rss.getRssBbcXQFilter(this.title, this.description,  this.category )
-        .then(newRss => {
-          if (newRss.length === undefined) {
-            this.rssBbcXQ = [];
-            this.rssBbcXQ.push(newRss);
-          }
-          else {
-            this.rssBbcXQ = newRss;
-          }
-          this.rssBbcXQLen = this.rssBbcXQ.length;
-
-        })
-        .then(undefined, (error) => { this.rssBbcXQ.length = 0; this.rssBbcXQLen = this.rssBbcXQ.length });
-
-
-
-
-    }
-
-
-  }
-
-
-
-  clean() {
-    this.title = "";
-    this.description = "";
-    this.category = "";
-    this.ngOnInit();
-
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BackService} from './provider/back.service';
-import { RssService} from './provider/rss.service';
+import { BackService } from './provider/back.service';
+import { RssService } from './provider/rss.service';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { RegexTitlePipe } from './pipe/regex-title.pipe';
 import { RegexCategoryPipe } from './pipe/regex-category.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProfesoresComponent } from './profesores/profesores.component';
+import { RssComponent } from './rss/rss.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const appRoutes: Routes = [
+  { path: 'Universidad', component: ProfesoresComponent },
+  { path: 'Rss', component: RssComponent },
+  { path: '**', component: ProfesoresComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     RegexTitlePipe,
-    RegexCategoryPipe
+    RegexCategoryPipe,
+    ProfesoresComponent,
+    RssComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
-    FormsModule      
+    FormsModule
   ],
   providers: [BackService, RssService, RegexTitlePipe, RegexCategoryPipe],
   bootstrap: [AppComponent]
